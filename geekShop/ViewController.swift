903bcee437af6c18d111e6ef50b574d15c45e718
Frostfell <ichernyshov@bk.ru>
2019-01-12 11:02:17 +0300

    Initial commit

---
+++ geekShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  geekShop
+//
+//  Created by Igor Chernyshov on 12/01/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ geekShopTests/geekShopTests.swift
@@ -0,0 +1,34 @@
+//
+//  geekShopTests.swift
+//  geekShopTests
+//
+//  Created by Igor Chernyshov on 12/01/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import XCTest
+@testable import geekShop
+
+class geekShopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
