e85f0c08173ebe507e54a35dd5fc652f13d2ddbf
Igor Chernyshov <ichernyshov@bk.ru>
2019-01-20 10:35:03 +0300

    Refactored NetworkServiceFactory
    Refactored NetworkServiceFactory, added Configuration file that contains constants

--- geekShop/App/Assembly/NetworkServiceFactory.swift
+++ geekShop/App/Assembly/NetworkServiceFactory.swift
@@ -11,13 +11,10 @@ import Alamofire
 
 class NetworkServiceFactory {
   
-  let baseUrl = URL(string: "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses")!
+  private let configuration = Configuration()
+  private let errorParser = ErrorParserImplementation()
   
-  func makeErrorParser() -> ErrorParser {
-    return ErrorParserImplementation()
-  }
-  
-  lazy var commonSessionManager: SessionManager = {
+  private(set) lazy var commonSessionManager: SessionManager = {
     let configuration = URLSessionConfiguration.default
     configuration.httpShouldSetCookies = false
     configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
@@ -25,26 +22,24 @@ class NetworkServiceFactory {
     return manager
   }()
   
-  func makeNetworkService() -> NetworkService {
-    return NetworkServiceImplementation(
-      errorParser: makeErrorParser(),
-      sessionManager: commonSessionManager
-    )
-  }
+  private(set) lazy var networkService = NetworkServiceImplementation(
+    errorParser: errorParser,
+    sessionManager: commonSessionManager
+  )
   
   func makeAuthService() -> AuthService {
-    return AuthServiceImplementation(baseURL: baseUrl, networkService: makeNetworkService())
+    return AuthServiceImplementation(baseURL: configuration.baseUrl, networkService: networkService)
   }
   
   func makeRegisterService() -> RegisterService {
-    return RegisterServiceImplementation(baseURL: baseUrl, networkService: makeNetworkService())
+    return RegisterServiceImplementation(baseURL: configuration.baseUrl, networkService: networkService)
   }
   
   func makeLogoutService() -> LogoutService {
-    return LogoutServiceImplementation(baseURL: baseUrl, networkService: makeNetworkService())
+    return LogoutServiceImplementation(baseURL: configuration.baseUrl, networkService: networkService)
   }
   
   func makeChangeUserDataService() -> ChangeUserDataService {
-    return ChangeUserDataServiceImplementation(baseURL: baseUrl, networkService: makeNetworkService())
+    return ChangeUserDataServiceImplementation(baseURL: configuration.baseUrl, networkService: networkService)
   }
 }
---
+++ geekShop/Environments/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configuration.swift
+//  geekShop
+//
+//  Created by Igor Chernyshov on 20/01/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+import Foundation
+
+struct Configuration {
+  let baseUrl = URL(string: "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses")!
+}
