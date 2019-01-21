cce4f85b3cde5240f6b4771829b8041f041f3fc0
Igor Chernyshov <ichernyshov@bk.ru>
2019-01-21 07:47:04 +0300

    Added general HTTP errors to ErrorParser

--- geekShop/Core/Network/ErrorParser.swift
+++ geekShop/Core/Network/ErrorParser.swift
@@ -10,7 +10,10 @@ import Foundation
 import Alamofire
 
 enum AppError: Error {
+  case authenticationError
+  case clientError
   case serverError
+  case unknownError
 }
 
 protocol ErrorParser {
@@ -24,13 +27,27 @@ class ErrorParserImplementation: ErrorParser {
     return result
   }
   
-  func parse(_ reauest: URLRequest?, _ response: HTTPURLResponse, _ data: Data?) -> Request.ValidationResult {
-    if !(200..<300).contains(response.statusCode) {
+  func parse(_ request: URLRequest?, _ response: HTTPURLResponse, _ data: Data?) -> Request.ValidationResult {
+    if data == nil {
+      // TODO: Show Alert to user "Looks like something is broken on our side. Please, try again later"
       return .failure(AppError.serverError)
-    } else if data == nil {
-      return .failure(AppError.serverError)
-    } else {
+    }
+    switch response.statusCode {
+    case 200..<300:
+      // Request was successful, data has been received. Continue application execution
       return .success
+    case 300..<400, 500...530:
+      // TODO: Show Alert to user "Looks like something is broken on our side. Please, try again later"
+      return .failure(AppError.serverError)
+    case 401, 403:
+      // TODO: Request was unsuccessful due to authentication. Try to re-obtain token or re-login user
+      return .failure(AppError.authenticationError)
+    case 400..<500:
+      // TODO: Show Alert to user "Cannot connect. Please check your Internet connection and try again"
+      return .failure(AppError.clientError)
+    default:
+      // TODO: Unhandled error. Send an error code to developers to create a handler
+      return .failure(AppError.unknownError)
     }
   }
   
