733289b5d125033702f532f427e7929aa57ffb38
Igor Chernyshov <ichernyshov@bk.ru>
2019-02-03 23:25:51 +0500

    Added tests for basket-related responses

--- geekShopTests/JSONResponsesTests.swift
+++ geekShopTests/JSONResponsesTests.swift
@@ -44,6 +44,8 @@ class JSONResponsesTests: XCTestCase {
     XCTFail("Successfully decoded stub JSON as \(T.self)")
   }
   
+  // MARK: - Review-related tests
+  
   func testAddReviewResponseDecodeSucceed() {
     successfullyDecode(json: "addReview", as: AddReviewResponse.self)
   }
@@ -67,6 +69,8 @@ class JSONResponsesTests: XCTestCase {
   func testRemoveReviewResponseDecodeFailed() {
     failToDecode(codable: RemoveReviewResponse.self)
   }
+  
+  // MARK: - User's account-related tests
   
   func testChangeUserProfileResponseDecodeSucceed() {
     successfullyDecode(json: "changeUserData", as: ChangeUserProfileResponse.self)
@@ -106,6 +110,72 @@ class JSONResponsesTests: XCTestCase {
   
   func testRegisterResponseDecodeFailed() {
     failToDecode(codable: RegisterResponse.self)
+  }
+  
+  // MARK: - Basket-related tests
+  
+  func testAddItemToBasketResponseDecodeSucceed() {
+    successfullyDecode(json: "addItemToBasket", as: AddItemToBasketResponse.self)
+  }
+  
+  func testAddItemToBasketResponseDecodeFailed() {
+    failToDecode(codable: AddItemToBasketResponse.self)
+  }
+  
+  func testSuccessfullyGotItemsListResponseDecodeSucceed() {
+    successfullyDecode(json: "successfullyGotItemsList", as: GetItemsListResponse.self)
+  }
+  
+  func testSuccessfullyGotItemsListResponseDecodeFailed() {
+    failToDecode(codable: GetItemsListResponse.self)
+  }
+  
+  func testFailedToGetItemsListResponseDecodeSucceed() {
+    successfullyDecode(json: "failedToGetItemsList", as: GetItemsListResponse.self)
+  }
+  
+  func testFailedToGetItemsListResponseDecodeFailed() {
+    failToDecode(codable: GetItemsListResponse.self)
+  }
+  
+  func testItemResponseDecodeSucceed() {
+    successfullyDecode(json: "item", as: Item.self)
+  }
+  
+  func testItemResponseDecodeFailed() {
+    failToDecode(codable: Item.self)
+  }
+  
+  func testSuccessfullyGotUsersBasketResponseDecodeSucceed() {
+    successfullyDecode(json: "successfullyGotUsersBasket", as: GetUsersBasketResponse.self)
+  }
+  
+  func testSuccessfullyGotUsersBasketResponseDecodeFailed() {
+    failToDecode(codable: GetUsersBasketResponse.self)
+  }
+  
+  func testFailedToGetUsersBasketResponseDecodeSucceed() {
+    successfullyDecode(json: "failedToGetUsersBasket", as: GetUsersBasketResponse.self)
+  }
+  
+  func testFailedToGetUsersBasketResponseDecodeFailed() {
+    failToDecode(codable: GetUsersBasketResponse.self)
+  }
+  
+  func testItemInBasketResponseDecodeSucceed() {
+    successfullyDecode(json: "itemInBasket", as: ItemInBasket.self)
+  }
+  
+  func testItemInBasketResponseDecodeFailed() {
+    failToDecode(codable: ItemInBasket.self)
+  }
+  
+  func testRemoveItemFromBasketResponseDecodeSucceed() {
+    successfullyDecode(json: "removeItemFromBasket", as: RemoveItemFromBasketResponse.self)
+  }
+  
+  func testRemoveItemFromBasketResponseDecodeFailed() {
+    failToDecode(codable: RemoveItemFromBasketResponse.self)
   }
   
 }
