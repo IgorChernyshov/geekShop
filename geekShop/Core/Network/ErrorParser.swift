509ed138f61de4b1d07609e6b2d45908d442fb4d
Igor Chernyshov <ichernyshov@bk.ru>
2019-02-04 22:08:21 +0500

    Replaced AppError comments with a description

--- geekShop/Core/Network/ErrorParser.swift
+++ geekShop/Core/Network/ErrorParser.swift
@@ -15,16 +15,18 @@ import Alamofire
  error codes that were returned as a response from the server.
  */
 
+/// <#Description#>
+///
+/// - authenticationError: The server returns an authentication error.
+/// - canceledRequest: Client has canceled a request.
+/// - clientError: A request didn't reach the server.
+/// - serverError: The server didn't return an error but the response has no data.
+/// - unknownError: This error is not specified. It will be reported to developers.
 enum AppError: Error {
-  // Server returns an authentication error
   case authenticationError
-  // Client has canceled a request
   case canceledRequest
-  // The request didn't reach the server
   case clientError
-  // Server didn't return an error but the response has no data
   case serverError
-  // This error is not specified. It will be reported to developers
   case unknownError
 }
 
@@ -32,17 +34,17 @@ protocol ErrorParser {
   
   /// This method parses errors that did not reach the server
   ///
-  /// - Parameter result: an error that is returned when a request cannot reach the server
-  /// - Returns: One of the errors that is handled by ErrorParser
+  /// - Parameter result: an error that is returned when a request cannot reach the server.
+  /// - Returns: One of the errors that is handled by ErrorParser.
   func parse(_ result: Error) -> AppError
   
   /// This method parses an answer that was received from the server
   ///
   /// - Parameters:
-  ///   - request: a request that was sent to the server
-  ///   - response: a response that was received from the server
-  ///   - data: data that was received from the server with a response
-  /// - Returns: A result of a request to the server
+  ///   - request: a request that was sent to the server.
+  ///   - response: a response that was received from the server.
+  ///   - data: data that was received from the server with a response.
+  /// - Returns: A result of a request to the server.
   func parse(_ request: URLRequest?, _ response: HTTPURLResponse, _ data: Data?) -> Request.ValidationResult
   
 }
@@ -64,20 +66,20 @@ class ErrorParserImplementation: ErrorParser {
   func parse(_ request: URLRequest?, _ response: HTTPURLResponse, _ data: Data?) -> Request.ValidationResult {
     switch response.statusCode {
     case 200..<300:
-      // This status code means success. Yet still need to check if we have received some data
+      // This status code means success. Yet still need to check if we have received some data.
       let dataIsEmpty = data == nil
       if dataIsEmpty {
-        // TODO: Show alert to user "Looks like something is broken on our side. Please, try again later"
+        // TODO: Show alert to user "Looks like something is broken on our side. Please, try again later".
         return .failure(AppError.serverError)
       } else {
-        // TODO: Request was successful, data has been received. Continue application execution
+        // TODO: Request was successful, data has been received. Continue application execution.
         return .success
       }
     case 401, 403:
-      // TODO: Request was unsuccessful due to authentication. Try to re-obtain token or re-login user
+      // TODO: Request was unsuccessful due to authentication. Try to re-obtain token or re-login user.
       return .failure(AppError.authenticationError)
     default:
-      // TODO: Unhandled error. Send an error code to developers to create a handler
+      // TODO: Unhandled error. Send an error code to developers to create a handler.
       return .failure(AppError.unknownError)
     }
   }
